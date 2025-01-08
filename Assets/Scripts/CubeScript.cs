8c005da324a7466a3952f7e61da9c6e3d167f68a
BenjaminLM95 <benjamin.lerner95@gmail.com>
2025-01-08 14:56:35 -0400

    Make the scripts cleaner

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -1,25 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
     private Rigidbody rb;
-    public Vector3 jump;
+    public Vector3 jump; 
+     
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
 
     public void moveCube()
     {
         rb.AddForce(5.0f * jump, ForceMode.Impulse);
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
-       
-    }
 
     private void OnEnable()
     {
--- Assets/Scripts/SphereScript.cs
+++ Assets/Scripts/SphereScript.cs
@@ -1,25 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SphereScript : MonoBehaviour
 {
     private Rigidbody rb;
-    public Vector3 jump;
-
-    public void moveSphere() 
-    {
-        rb.AddForce(2.5f * jump, ForceMode.Impulse); 
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 jump; 
+    
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 0.0f, 3.0f);
-        
     }
+
+    public void moveSphere() 
+    {
+       rb.AddForce(2.5f * jump, ForceMode.Impulse); 
+    }
+     
 
     private void OnEnable()
     {
